90b0482f31875068c7279908cb08d67c080f57c3
rfloresl <r.flores.l@gmail.com>
2021-01-20 12:29:53 -0300

    Agrega sale_order_currency y l10n_cl_erps21

---
+++ sale_order_currency/models/sale_order.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models, api
+from odoo.tools.translate import _
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+    
+    convert_to_company_currency = fields.Boolean(u'Convertir a moneda local?')
+
+    def _prepare_invoice(self):
+        invoice_vals = super(SaleOrder, self)._prepare_invoice()
+        if self.convert_to_company_currency:
+            invoice_vals.update({
+                'currency_id': self.company_id.currency_id.id,
+            })
+        return invoice_vals
