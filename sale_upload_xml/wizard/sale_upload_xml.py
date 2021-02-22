69d685e8053d58c79a523619b6804ad790acca0a
Richard Flores <r.flores.l@gmail.com>
2021-02-22 18:28:53 -0300

    sale_upload_xml

--- sale_upload_xml/wizard/sale_upload_xml.py
+++ sale_upload_xml/wizard/sale_upload_xml.py
@@ -62,23 +62,60 @@ class SaleUploadXml(models.TransientModel):
                 precio = linea.xpath('PricingDetail/ListOfPrice/Price/UnitPrice/UnitPriceValue')[0].text
                 # Agrega al detalle
                 detalle.append({
-                    'product_id': product_id,
-                    'qty_ordered': cantidad,
-                    'price': precio
+                    'product_id': product_id[0].id,
+                    'product_uom_qty': cantidad,
+                    'price_unit': precio,
                 })
             # Agrega a las ordenes a crear
             aCrear.append({
-                'company_id': company_id,
-                'partner_id': partner_id,
-                'order_lines': detalle
+                'company_id': company_id[0].id,
+                'partner_id': partner_id[0].id,
+                'detalle': detalle,
             })
         if not errores:
-            try:
-                self.env['sale.sale.order'].create(aCrear)
-            except:
-                raise UserError('Error al generar las ordenes de venta')
+            #try:
+            generadas = ""
+            for cabecera in aCrear:
+                datos = {
+                    "company_id": cabecera["company_id"],
+                    "partner_id": cabecera["partner_id"],
+                    "partner_invoice_id": cabecera["partner_id"],
+                    "partner_shipping_id": cabecera["partner_id"],
+                }
+                po = self.env['sale.order'].create(datos)
+                generadas = generadas + " " + po["name"]
+                for linea in cabecera["detalle"]:
+                    datos = {
+                        "order_id": po["id"],
+                        "product_id": linea["product_id"],
+                        "product_uom_qty": linea["product_uom_qty"],
+                        "price_unit": linea["price_unit"],
+                    }
+                    pol = self.env['sale.order.line'].create(datos)
+
+            # Mensaje de confirmacion
+            message_id = self.env['message.wizard'].create({'message': ("Se ha creado el(los) presupuesto(s)" + generadas)})
+            return {
+                'name': 'Subida exitosa',
+                'type': 'ir.actions.act_window',
+                'view_mode': 'form',
+                'res_model': 'message.wizard',
+                'res_id': message_id.id,
+                'target': 'new'
+            }
+            #except:
+            #    raise UserError('Error al generar las ordenes de venta')
         else:
             elError = "Se han generado los siguientes errores:\n"
             for error in errores:
                 elError = elError + "- " + error + "\n"
             raise UserError(elError)
+
+class MessageWizard(models.TransientModel):
+    _name = 'message.wizard'
+
+    message = fields.Text('Message', required=True)
+
+    def action_ok(self):
+        """ close wizard"""
+        return {'type': 'ir.actions.act_window_close'}
